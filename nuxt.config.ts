d3f31ebe7d3e2e4030c9d00f831d6deb328541b6
Lis1459 <Lis1459@yandex.ru>
2026-04-14 19:07:24 +0300

    chore: install primeVue forms and icons dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@pinia/nuxt", "@primevue/nuxt-module"],
+  css: ["primeicons/primeicons.css"],
+
   typescript: {
     typeCheck: true,
   },
