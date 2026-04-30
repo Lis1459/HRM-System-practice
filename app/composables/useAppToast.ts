f7449dc3c4e9174e02890d66f432e90a62202091
Lis1459 <Lis1459@yandex.ru>
2026-04-30 16:24:00 +0300

    feat: change toast live display

--- app/composables/useAppToast.ts
+++ app/composables/useAppToast.ts
@@ -5,9 +5,19 @@ export const useAppToast = () => {
 
   return {
     successToast: (message: string) =>
-      toast.add({ severity: "success", summary: "Success", detail: message }),
+      toast.add({
+        severity: "success",
+        summary: "Success",
+        detail: message,
+        life: 3000,
+      }),
 
     errorToast: (message: string) =>
-      toast.add({ severity: "error", summary: "Error", detail: message }),
+      toast.add({
+        severity: "error",
+        summary: "Error",
+        detail: message,
+        life: 3000,
+      }),
   };
 };
